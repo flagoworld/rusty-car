240c922a7287382bff109dd4deb2bf6fc949900a
Ryan Layne <rlayne.ca@gmail.com>
2015-07-12 07:59:12 +0000

    Add temporary zero handler

---
+++ core/src/handlers/zero.rs
@@ -0,0 +1,24 @@
+use lin::frame::LINFrameHandler;
+
+pub struct Zero;
+
+impl Zero
+{
+	pub fn new() -> Zero
+	{
+		Zero
+	}
+}
+
+impl LINFrameHandler for Zero
+{
+    fn response_data(&self) -> Vec<u8>
+    {
+		return vec![1, 2, 3];
+    }
+
+	fn handle_response(&self, data: Vec<u8>)
+	{
+		println!("HANDLE: {:?}", data);
+	}
+}

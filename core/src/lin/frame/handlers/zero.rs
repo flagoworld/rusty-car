d46e7a49ba3b049195564f1b4c685f2921e8eff2
Ryan <rlayne.ca@gmail.com>
2015-07-13 16:06:34 -0700

    Recommit moved files

---
+++ core/src/lin/frame/handlers/zero.rs
@@ -0,0 +1,29 @@
+use lin::frame::LINFrameHandler;
+
+pub struct Zero;
+
+impl Zero
+{
+	pub fn new() -> Zero
+	{
+		Zero
+	}
+}
+
+impl LINFrameHandler for Zero
+{
+	fn boxed_new(&self) -> Box<LINFrameHandler>
+	{
+		Box::new(Zero::new())
+	}
+
+    fn response_data(&self) -> Vec<u8>
+    {
+		return vec![1, 2, 3];
+    }
+
+	fn handle_response(&self, data: &Vec<u8>)
+	{
+		//println!("HANDLE: {:?}", data);
+	}
+}

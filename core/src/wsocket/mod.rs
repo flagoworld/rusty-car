a156d2eaf37e0d108c2c98759dc50c8c7f8cdbc1
Ryan <rlayne.ca@gmail.com>
2015-07-20 23:21:33 -0700

    Doesnt compile. Exposed wsocket::route

--- core/src/wsocket/mod.rs
+++ core/src/wsocket/mod.rs
@@ -1,33 +1,37 @@
-// use websocket::{Message, Sender};
-// use std::io::Write;
-// use websocket::stream::WebSocketStream;
-//
-// pub fn route<D>(sender: &mut Sender<D>, message: Message)
-// {
-//     match message
-//     {
-//         Message::Close(_) =>
-//         {
-//             let message = Message::Close(None);
-//             sender.send_message(message).unwrap();
-//             println!("Client disconnected");
-//             return;
-//         }
-//         Message::Ping(data) =>
-//         {
-//             let message = Message::Pong(data);
-//             sender.send_message(message).unwrap();
-//         }
-//         // Message::Text(data) =>
-//         // {
-//         //     let message = Message::Text(match &data
-//         //     {
-//         //         "mph" => "12",
-//         //         "rpm" => "1350"
-//         //     });
-//         //
-//         //     sender.send_message(message).unwrap();
-//         // }
-//         _ => sender.send_message(message).unwrap(),
-//     }
-// }
+use websocket::{Message, Sender};
+use std::io::Write;
+use websocket::stream::WebSocketStream;
+use rand;
+
+pub fn route(sender: &mut Sender<WebSocketStream>, message: Message)
+{
+    match message
+    {
+        Message::Close(_) =>
+        {
+            let message = Message::Close(None);
+            sender.send_message(message).unwrap();
+            println!("Client disconnected");
+            return;
+        }
+        Message::Ping(data) =>
+        {
+            let message = Message::Pong(data);
+            sender.send_message(message).unwrap();
+        }
+        Message::Text(data) =>
+        {
+            let mph: u8 = (rand::random::<u8>() % 100) + 1;
+            let rpm: u16 = (rand::random::<u16>() % 8000) + 1;
+            let message = Message::Text(match data.as_ref()
+            {
+                "mph" => format!("mph:{}", mph),
+                "rpm" => format!("rpm:{}", rpm),
+                _ => return
+            }.to_string());
+
+            sender.send_message(message).unwrap();
+        }
+        _ => sender.send_message(message).unwrap(),
+    }
+}
